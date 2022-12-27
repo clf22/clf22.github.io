53039b25e901a59dd31515caa01673f53d373298
carlosLF <carlosluengofajardo@gmail.com>
2022-12-27 20:53:21 +0100

    First commit

---
+++ data.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        "titulo": "Ayesa 2022",
+        "texto": "Backend"
+    },
+    {
+        "titulo": "Ayesa 2020",
+        "texto": "RPA"
+    },
+    {
+        "titulo": "Ayesa 2017",
+        "texto": "sap"
+    }
+]
