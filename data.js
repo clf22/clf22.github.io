25bfefe9ddb72601d6982832fa218cf399d91f43
carlosLF <carlosluengofajardo@gmail.com>
2022-12-30 13:28:59 +0100

    + Cambio en textos de experiencia

--- data.js
+++ data.js
@@ -6,10 +6,11 @@ export default [
     "experiencia": [{
       "titulo": "Desarrollador Fullstacks",
       "explicacion": `
-        Mis tareas principales se dedican al desarrollo Backend, realizando microservicios API REST que proporcionen al Frontend los datos necesarios para su correcto funcionamiento.
+        Mis tareas principales se enfocan en el desarrollo Backend, realizando microservicios API REST que proporcionen al Frontend los datos necesarios para su correcto funcionamiento.
         Para el procesamiento de los datos, principalmente trabajo con bases de datos relacionales (MariaDB o PostgreSQL) o no relacionales (MongoDB) mediante el uso de Sequelize.
-        Otro métodos de procesamiento de datos con los que he trabajado son la lectura de ficheros Excels y correos electrónicos (recepción/envios).
-        También he podido realizar pequeños desarollos Frontend con Vue, desde pequeños formularios a aplicaciones más complejas para el visionado de informes. Para realizar las comunicaciones con el servidor utilizo Axios.
+        Por otro lado, he trabajado con otro métodos de procesamiento de datos como lectura de ficheros Excels y correos electrónicos (recepción/envios).
+        Otro de mis cometidos ha sido la realización de pequeños desarollos Frontend con Vue, desde formularios simples a aplicaciones más complejas para el visionado de informes. Para realizar las comunicaciones con el servidor utilizo Axios.
+        Por último, la ocupación a la que le dedico menos tiempo a día de hoy, es la automatización de procesos. Ahora más centrado en la automatización de aplicaciones web mediante el uso de Puppeter.
       `,
       "inicio": "01/11/2019",
       "fin": "now",
@@ -18,12 +19,12 @@ export default [
     {
       "titulo": "Analista/Desarrollador RPA",
       "explicacion": `
-        Actualmente mis tareas en referente a la automatización de procesos se basan en ayudar a la toma de requisitos con cliente y redactar los documentos funcionales para que el equipo de desarrollo puedan iniciar los proyectos.
-        Mis tareas se dedicaban al desarrollo de procesos automatizados con la herramienta UiPath, trabajando en sistemas como SAP, CRM, Salesforces y applicaciones web y de escritorio en general.
-        Para el procesamiento de los datos de entrada, normalmente se trabajaba con Excel, correos, carpetas compartidas, FTP/SFTP. 
-        Durante el proceso del RPA se gestionaban los datos en una base de dato SQL Server y posteriormente se extraia la información de esta para poder generar los informes que se enviaban por correo electrónico al cliente para su posterior revisión.
-        He trabajado con varios desarrolladores a mi cargo, revisando las tareas que realizaban, el control de los errores y ayudandolos con las dudas técnicas.
-        Otras herramientas de automatización que he utilizado han sido puppeteer, robotframework y selenium.
+        Actualmente, mis tareas, en referente a la automatización de procesos, se basan en ayudar a la toma de requisitos con cliente y redactar los documentos funcionales para que el equipo de desarrollo pueda iniciar los proyectos. Estas las compagino con mi ocupación principal a día de hoy: el desarrollo de aplicaciones web.
+        Anteriormente, mis tareas se dedicaban al desarrollo de procesos automatizados con la herramienta UiPath, trabajando en sistemas como SAP, CRM, Salesforces y applicaciones web y de escritorio en general.
+        Para el procesamiento de los datos de entrada, normalmente se trabajaba con Excel, correos, carpetas compartidas y FTP/SFTP. 
+        Durante el proceso del RPA se gestionaban los datos en una base de dato SQL Server y posteriormente se extraía la información de esta para poder generar los informes que se enviaban por correo electrónico al cliente para su posterior revisión.
+        He trabajado con varios desarrolladores a mi cargo, revisando las tareas que realizaban, el control de los errores y ayudándolos con las dudas técnicas.
+        Otras herramientas de automatización que he utilizado han sido Puppeteer, Robotframework y Selenium.
       `,
       "inicio": "25/10/2017",
       "fin": "now",
