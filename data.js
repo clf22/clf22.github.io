dea3175619b77a9959c4da6f3628609917477261
carlosLF <carlosluengofajardo@gmail.com>
2022-12-30 01:18:12 +0100

    + Experiencias profesionales recientes

---
+++ data.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    "empresa": "Ayesa 2022",
+    "inicio": "25/10/2017",
+    "fin": "now",
+    "experiencia": [{
+      "titulo": "Desarrollador Fullstacks",
+      "explicacion": `
+        Mis tareas principales se dedican al desarrollo Backend, realizando microservicios API REST que proporcionen al Frontend los datos necesarios para su correcto funcionamiento.
+        Para el procesamiento de los datos, principalmente trabajo con bases de datos relacionales (MariaDB o PostgreSQL) o no relacionales (MongoDB) mediante el uso de Sequelize.
+        Otro métodos de procesamiento de datos con los que he trabajado son la lectura de ficheros Excels y correos electrónicos (recepción/envios).
+        También he podido realizar pequeños desarollos Frontend con Vue, desde pequeños formularios a aplicaciones más complejas para el visionado de informes. Para realizar las comunicaciones con el servidor utilizo Axios.
+      `,
+      "inicio": "01/11/2019",
+      "fin": "now",
+      "tecnologia": ['nodejs', 'nestjs', 'vuejs', 'mariadb', 'mongodb', 'postgresql', 'html5', 'css', 'js']
+    },
+    {
+      "titulo": "Analista/Desarrollador RPA",
+      "explicacion": `
+        Actualmente mis tareas en referente a la automatización de procesos se basan en ayudar a la toma de requisitos con cliente y redactar los documentos funcionales para que el equipo de desarrollo puedan iniciar los proyectos.
+        Mis tareas se dedicaban al desarrollo de procesos automatizados con la herramienta UiPath, trabajando en sistemas como SAP, CRM, Salesforces y applicaciones web y de escritorio en general.
+        Para el procesamiento de los datos de entrada, normalmente se trabajaba con Excel, correos, carpetas compartidas, FTP/SFTP. 
+        Durante el proceso del RPA se gestionaban los datos en una base de dato SQL Server y posteriormente se extraia la información de esta para poder generar los informes que se enviaban por correo electrónico al cliente para su posterior revisión.
+        He trabajado con varios desarrolladores a mi cargo, revisando las tareas que realizaban, el control de los errores y ayudandolos con las dudas técnicas.
+        Otras herramientas de automatización que he utilizado han sido puppeteer, robotframework y selenium.
+      `,
+      "inicio": "25/10/2017",
+      "fin": "now",
+      "tecnologia": ['uipath', 'sqlserver', 'excel', 'web api', 'citrix', 'correo', 'puppeteer', 'selenium', 'robotframework']
+    }]
+  }
+]
